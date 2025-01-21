5f5df09a8afac323911c2a7ca2a79ce27cfcc075
alexey <fostiriy@ya.ru>
2025-01-21 16:55:39 +1000

    Add makefiles

--- lab_4/parallel_mod/vector_mod.cpp
+++ lab_4/parallel_mod/vector_mod.cpp
@@ -8,48 +8,53 @@
 
 using namespace std;
 
+// Функция для возведения числа в степень по модулю
 IntegerWord pow_mod(IntegerWord base, IntegerWord power, IntegerWord mod) {
     IntegerWord result = 1;
     while (power > 0) {
         if (power % 2 != 0) {
-            result = mul_mod(result, base, mod);
+            result = mul_mod(result, base, mod); // Умножение по модулю, если степень нечетная
         }
-        power >>= 1;
-        base = mul_mod(base, base, mod);
+        power >>= 1; // Деление степени на 2
+        base = mul_mod(base, base, mod); // Возведение основания в квадрат по модулю
     }
     return result;
 }
 
+// Функция для возведения числа в степень по модулю с отрицательным основанием
 IntegerWord word_pow_mod(std::size_t power, IntegerWord mod) {
     return pow_mod((-mod) % mod, power, mod);
 }
 
+// Структура для хранения диапазона задач для потока
 struct thread_range {
-    size_t b, e;
+    size_t b, e; // Начало и конец диапазона
 };
 
-thread_range vector_thread_range(size_t N, size_t T, size_t t) {    
-    auto b = N % T;
-    auto s = N / T;
+// Функция для вычисления диапазона задач для потока
+thread_range vector_thread_range(size_t N, size_t T, size_t t) {
+    auto b = N % T; // Вычисление начального индекса
+    auto s = N / T; // Вычисление размера задачи для каждого потока
 
     if (t < b) 
-        b = ++s * t;
+        b = ++s * t; // Корректировка начального индекса для первых потоков
     else 
-        b = s * t + b;
+        b = s * t + b; // Корректировка начального индекса для остальных потоков
 
-    size_t e = b + s;
+    size_t e = b + s; // Вычисление конечного индекса
 
-    return thread_range(b, e);
+    return thread_range{b, e}; // Возвращение диапазона задач
 }
 
 // Защита от ложного выделения
 struct partial_result_t {
-    alignas(std::hardware_destructive_interference_size) IntegerWord value;
+    alignas(std::hardware_destructive_interference_size) IntegerWord value; // Выравнивание для предотвращения ложного выделения
 };
 
+// Функция для вычисления модуля вектора
 IntegerWord vector_mod(const IntegerWord* V, size_t N, IntegerWord mod){
-    unsigned T = get_num_threads();
-    vector<thread> threads(T-1);
+    unsigned T = get_num_threads(); // Получение количества потоков
+    vector<thread> threads(T-1); // Вектор для хранения потоков
     vector<partial_result_t> partial_results(T); //учитываем, что главный поток выполняет лямбду с t = 0
     IntegerWord S = 0;
     barrier<> bar(T);
@@ -64,7 +69,7 @@ IntegerWord vector_mod(const IntegerWord* V, size_t N, IntegerWord mod){
            //sum = (sum * x + V[e-1-i]) % mod;
             sum = add_mod(times_word(sum, mod), V[--i], mod); // то же самое, но без переполнения
 
-        // ... код про общую сумму
+        // Сохранение частичного результата
         partial_results[t].value = sum;
         for (size_t i = 1, ii = 2; i < T; i = ii, ii += ii) {
             bar.arrive_and_wait();
